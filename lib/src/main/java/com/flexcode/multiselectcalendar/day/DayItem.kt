8405f7e0c836443d32b0eb69de3967f6e79c94d8
evgeniy1215 <evgeniy1215@gmail.com>
2025-01-22 22:06:24 +0300

    Update DayItem.kt

--- lib/src/main/java/com/flexcode/multiselectcalendar/day/DayItem.kt
+++ lib/src/main/java/com/flexcode/multiselectcalendar/day/DayItem.kt
@@ -37,7 +37,7 @@ public fun <T : ItemSelectState> DayItem(
     modifier: Modifier = Modifier,
     showCurrentDay: Boolean = false,
     selectedColor: Color = MaterialTheme.colorScheme.background,
-    currentDayColor: Color = Color(0xFF03B8F1),
+    currentDayColor: Color = Color(0xFF07DA624),
     onClick: (LocalDate) -> Unit = {},
 ) {
     val date = state.date
@@ -59,7 +59,7 @@ public fun <T : ItemSelectState> DayItem(
             null
         },
         colors = CardDefaults.cardColors(
-            containerColor = if (isSelected) Color(0xFF03B8F1) else selectedColor,
+            containerColor = if (isSelected) Color(0xFF07DA624) else selectedColor,
         ),
         shape = CircleShape,
     ) {
